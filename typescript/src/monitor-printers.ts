f13ad5f2da380c4834456ad462c28f017f6c4bae
Daniel Schiavini <d.schiavini@ultimaker.com>
2021-12-13 15:50:17 +0100

    Documentation & self-review

--- typescript/src/monitor-printers.ts
+++ typescript/src/monitor-printers.ts
@@ -5,29 +5,17 @@ import {
 import { DigitalFactoryDemo } from './digital-factory';
 import { prettyJSON, print } from './print';
 
+/**
+ * Contains the used fields from the Connect API cluster response.
+ */
 interface ClusterResponse {
-    capabilities?: string[];
     cluster_id: string;
-    display_status?: 'error' | 'idle' | 'maintenance' | 'pre_print' | 'printing' | 'unknown' | 'unreachable' | 'offline' | 'disabled' | 'updating_firmware';
-    friendly_name: string;
-    host_current_print_job?: any;
-    host_guid: string;
-    host_internal_ip?: string;
-    host_name: string;
-    host_print_job_count?: number;
-    host_printer?: any;
-    host_remaining_print_time?: number;
-    host_version?: string;
     is_online: boolean;
-    organization_id?: string;
-    organization_shared?: boolean;
-    printer_count: number;
-    printer_type?: 'ultimaker2_plus_connect' | 'ultimaker3' | 'ultimaker3_extended' | 'ultimaker_s3' | 'ultimaker_s5';
-    status: 'active' | 'inactive';
-    team_ids?: string[];
-    user_id?: string;
 }
 
+/**
+ * Describes the comparison between two lists of clusters.
+ */
 interface Comparison {
     dateTime: string;
     appeared: string[];
@@ -36,6 +24,31 @@ interface Comparison {
     offlineCount: number;
 }
 
+/**
+ * The options that may be given to this script.
+ */
+interface MonitorClustersOptions {
+    comparisonFile?: string;
+    statusDir?: string;
+    waitMS?: number;
+}
+
+/**
+ * Gets the previous cluster status from the status log directory. Ensures that the directory actually exists.
+ */
+async function getPreviousClusterStatus(statusDir: string): Promise<ClusterResponse[] | null> {
+    if (!existsSync(statusDir)) {
+        await mkdir(statusDir, { recursive: true });
+        return null;
+    }
+    const existingFiles = await readdir(statusDir);
+    const lastFile = existingFiles.filter((file) => file.endsWith('.json')).sort((a, b) => b.localeCompare(a))[0];
+    return lastFile && JSON.parse(await readFile(`${statusDir}/${lastFile}`, 'utf-8'));
+}
+
+/**
+ * Compares two lists of cluster statuses.
+ */
 function compareResults(previous: ClusterResponse[], current: ClusterResponse[], dateTime: Date): Comparison {
     const getOnlineClusterIds = <T, K extends keyof any>(clusters: ClusterResponse[]): Set<string> => new Set<string>(
         clusters.filter((cluster) => cluster.is_online).map((cluster) => cluster.cluster_id),
@@ -51,28 +64,17 @@ function compareResults(previous: ClusterResponse[], current: ClusterResponse[],
     };
 }
 
-function csvLine(comparison: Comparison): string {
+/**
+ * Creates a CSV line from the given comparison.
+ */
+function csvLine(comparison: Comparison, separator = ';'): string {
     const fields: (keyof Comparison)[] = ['dateTime', 'appeared', 'gone', 'onlineCount', 'offlineCount'];
-    return fields.map((field) => comparison[field]).join(';');
+    return fields.map((field) => comparison[field]).join(separator);
 }
 
-interface MonitorClustersOptions {
-    comparisonFile?: string;
-    statusDir?: string;
-    waitMS?: number;
-}
-
-async function getPreviousClusterStatus(statusDir: string): Promise<ClusterResponse[] | null> {
-    if (!existsSync(statusDir)) {
-        await mkdir(statusDir, { recursive: true });
-        return null;
-    }
-    const existingFiles = await readdir(statusDir);
-    const lastFile = existingFiles.filter((file) => file.endsWith('.json')).sort((a, b) => b.localeCompare(a))[0];
-    return lastFile && JSON.parse(await readFile(`${statusDir}/${lastFile}`, 'utf-8'));
-}
-
-/* eslint-disable no-await-in-loop */
+/**
+ * Gets all clusters from the API periodically, writing the response to a JSON file & the comparisons to a CSV file.
+ */
 async function monitorClusters({
     comparisonFile = '../cluster-monitoring-logs/cluster-monitoring.csv',
     waitMS = 60000,
@@ -90,8 +92,9 @@ async function monitorClusters({
     let previous = await getPreviousClusterStatus(statusDir);
     const stream = createWriteStream(comparisonFile, { flags: 'a' });
 
+    /* eslint-disable no-await-in-loop */
     while (!stop) {
-        print('Creating demo project...');
+        print('Retrieving clusters...');
         const clusters = await demo.getClusters();
         const dateTime = new Date();
         const logFile = `${statusDir}/${dateTime.toISOString().replace(/:/g, '-')}-clusters.json`;
